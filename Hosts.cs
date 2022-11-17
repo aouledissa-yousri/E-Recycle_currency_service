148d47af4ae41b24636763993a4f357c8f2462f1
aouledissa <aouledissayousri@gmail.com>
2022-11-17 20:46:54 +0100

    added coin gains when making recycle request

--- Hosts.cs
+++ Hosts.cs
@@ -2,5 +2,7 @@ namespace E_Recycle_currency_service;
 
 public abstract class Hosts {
 
-    public static string coreUrl = "http://localhost:8000/core";
+    //public static string coreUrl = "http://localhost:8000/core";
+    public static string coreUrl = "https://e-recyclecoreservice-production.up.railway.app/core";
+    
 }

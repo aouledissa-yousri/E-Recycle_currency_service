b706ca0be15d2324735c16a22694398074d92feb
aouledissa <aouledissayousri@gmail.com>
2022-11-21 18:12:19 +0100

    last version

--- Hosts.cs
+++ Hosts.cs
@@ -3,7 +3,6 @@ namespace E_Recycle_currency_service;
 public abstract class Hosts {
 
     //public static string coreUrl = "http://localhost:8000/core";
-    //public static string coreUrl = "http://localhost:8000/core";
-    public static string coreUrl = "https://e-recyclecoreservice-production.up.railway.app/core";
+    public static string coreUrl = "https://e-recyclecoreservice-production-ffdc.up.railway.app/core";
     
 }
